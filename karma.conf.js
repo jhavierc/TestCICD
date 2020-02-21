a21df4af17ba73654fe243f2410a2316c3b5d589
Carlos Cepeda <cacepeda@personalsoft.com.co>
2020-02-21 14:42:10 -0500

    Prueba cambio 3 CI

--- karma.conf.js
+++ karma.conf.js
@@ -16,12 +16,30 @@ module.exports = function (config) {
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
+    files: [
+      { pattern: './src/test.ts', watched: false }
+    ],
+    preprocessors: {
+      './src/test.ts': ['@angular/cli']
+    },
+    mime: {
+      'text/x-typescript': ['ts', 'tsx']
+    },
+    //coverageIstanbulReporter: {
+    //  dir: require('path').join(__dirname, './coverage/AppTest'),
+    //  reports: ['html', 'lcovonly', 'text-summary'],
+    //  fixWebpackSourcePaths: true
+    //},
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/AppTest'),
-      reports: ['html', 'lcovonly', 'text-summary'],
+      reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    angularCli: {
+      environment: 'dev'
+    },
+    eporters: config.angularCli && config.angularCli.codeCoverage
+      ? ['progress', 'coverage-istanbul']
+      : ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
